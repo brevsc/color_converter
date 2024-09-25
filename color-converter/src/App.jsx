e5548de4559d97ded5a1306ee90a9b14269adfd4
Breno Vasconcelos <theyukidjr@gmail.com>
2024-09-25 11:20:51 -0300

    feat: add credits

--- color-converter/src/App.jsx
+++ color-converter/src/App.jsx
@@ -8,6 +8,7 @@ export default function App() {
         <h2 className="text-center text-6xl mb-2 mt-10 ">Conversor de Cores</h2>
         <Separator.Root className="bg-slate-400 w-56 h-[1px] rounded-sm" decorative />
         <ColorInputs/>
+        <p><a target="_blank" href="https://github.com/brevsc">Breno Vasconcelos</a> - 2024</p>
       </div>
     </div>
   );
